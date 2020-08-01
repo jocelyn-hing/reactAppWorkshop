d76302756ae36949ac49fe0c19d526fde1cbd7cf
Everyhings-Alright <jecylhing@gmail.com>
2020-08-01 17:07:15 +1000

    Added buttons to increment and decrement count. Note: need to fix negative subs

--- youtube-clone/src/App.js
+++ youtube-clone/src/App.js
@@ -4,7 +4,6 @@ import './App.css';
 class SubscriberCounter extends Component {
   render() {
     const {numSubscribers}= this.props;
-
     return (
     <h3>
       You have {numSubscribers} {numSubscribers === 1 ? ' subscriber' : 'subscribers'}!
@@ -14,12 +13,53 @@ class SubscriberCounter extends Component {
 
 }
 
+
+
+class AddRemoveSubscriber extends Component {
+  render() {
+    const {numSubscribers, onSubscriberUpdate}= this.props;
+    return (
+      <div>
+        <button 
+          onClick={() => onSubscriberUpdate(numSubscribers + 1)}
+        >
+          Subscribe!
+        </button>
+        <button 
+          onClick={() => onSubscriberUpdate(numSubscribers - 1)}
+        >
+          Unsubscribe
+        </button>
+    </div>
+    );
+  }
+}
+
+
 class App extends Component{
+
+  constructor() {
+    super();
+    this.handleSubscriberUpdate = this.handleSubscriberUpdate.bind(this);
+    this.state = { numSubscribers: 0 };
+  }
+
+  handleSubscriberUpdate(numSubscribers) {
+    this.setState({
+      numSubscribers
+    });
+  }
+
   render () {
+    const { numSubscribers} = this.state;
     return (
       <div className="App">
         <h1>Welcome to YooTube.</h1>
-        <SubscriberCounter numSubscribers={0}/>
+        <SubscriberCounter numSubscribers={numSubscribers}/>
+        <AddRemoveSubscriber 
+          numSubscribers={numSubscribers}
+          onSubscriberUpdate={this.handleSubscriberUpdate}
+        />
       </div>
     );
   }
