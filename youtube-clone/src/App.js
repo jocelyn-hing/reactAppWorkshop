d2f6ecb7ad50d9ef5f99a04f3d20c72968d93cc6
Everyhings-Alright <jecylhing@gmail.com>
2020-08-01 16:36:16 +1000

    Added subscriber count class

--- youtube-clone/src/App.js
+++ youtube-clone/src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class SubscriberCounter extends Component {
+  render() {
+    const {numSubscribers}= this.props;
+
+    return (
+    <h3>
+      You have {numSubscribers} {numSubscribers === 1 ? ' subscriber' : 'subscribers'}!
+    </h3>
+    );
+  }
+
+}
+
+class App extends Component{
+  render () {
+    return (
+      <div className="App">
+        <h1>Welcome to YooTube.</h1>
+        <SubscriberCounter numSubscribers={0}/>
+      </div>
+    );
+  }
+
 }
 
 export default App;
